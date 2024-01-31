67619b37eb4795ed67e3b47a38fd2386451a0fa8
kristianfiliposki <krisplay@outlook.it>
2024-01-31 22:15:14 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="jumbotron p-5 mb-4 bg-light rounded-3">
+    <div class="container py-5">
+        procedi con il 
+    </div>
+</div>
+
+@endsection
